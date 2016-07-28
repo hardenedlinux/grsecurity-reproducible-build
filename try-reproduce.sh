50c5bc21bd90e15e652bebfef1bcb0f07e06aa72
Icenowy Zheng <icenowy@aosc.xyz>
2016-07-28 17:18:59 +0800

    try-reproduce: use deb-diff
    Signed-off-by: Icenowy Zheng <icenowy@aosc.xyz>

--- try-reproduce.sh
+++ try-reproduce.sh
@@ -4,6 +4,13 @@ NO_ENTER_FIXED_DIR=1
 . fixed-dir.sh
 unset NO_ENTER_FIXED_DIR
 
-rm -rf out out0 $FIXED_DIRECTORY/linux-4.6.4{,-old}
+rm -rf out out0 $FIXED_DIRECTORY/linux-4.6.4{,-old} deb deb0
 ./build-kernel.sh && mv $FIXED_DIRECTORY/linux-4.6.4{,-old} && mv out out0 && ./build-kernel.sh
+mkdir deb deb0
+mv out/*.deb deb/
+mv out0/*.deb deb0/
 diff -r out{0,}
+for i in deb/*.deb
+do
+	./deb-diff.sh "$i" deb0/"$(basename "$i")"
+done
