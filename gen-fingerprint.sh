ac0aa55d753e07c902ababcbdcef229fa281e540
Icenowy Zheng <icenowy@aosc.xyz>
2016-08-01 10:59:54 +0800

    add comments for gen-fingerprint.sh
    Signed-off-by Icenowy Zheng <icenowy@aosc.xyz>

--- gen-fingerprint.sh
+++ gen-fingerprint.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 export TZ=UTC
+# The timestamp of kernel building, used for the embedded initramfs, /proc/version and kernel build
 echo "KERNEL_TIMESTAMP=\"$(LC_ALL=C date)\"" > fingerprint.sh
+# The seed used for Grsecurity's RANDSTRUCT plugin.
 echo "GRSEC_RANDSTRUCT_SEED=\"`od -A n -t x8 -N 32 /dev/urandom | tr -d ' \n'`\"" >> fingerprint.sh
