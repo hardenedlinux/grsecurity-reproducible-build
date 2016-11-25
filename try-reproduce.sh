efab6be6571f7ed8e236cb3b7801106332f48472
citypw <citypw@gmail.com>
2016-11-25 18:48:13 +0800

    update to grsecurity-3.1-4.8.10-201611232213.patch

--- try-reproduce.sh
+++ try-reproduce.sh
@@ -2,10 +2,11 @@
 
 NO_ENTER_FIXED_DIR=1
 . fixed-dir.sh
+. ver.sh
 unset NO_ENTER_FIXED_DIR
 
-rm -rf out out0 $FIXED_DIRECTORY/linux-4.6.4{,-old} deb deb0
-./build-kernel.sh && mv $FIXED_DIRECTORY/linux-4.6.4{,-old} && mv out out0 && ./build-kernel.sh
+rm -rf out out0 $FIXED_DIRECTORY/linux-$LINUX_VER{,-old} deb deb0
+./build-kernel.sh && mv $FIXED_DIRECTORY/linux-$LINUX_VER{,-old} && mv out out0 && ./build-kernel.sh
 mkdir deb deb0
 mv out/*.deb deb/
 mv out0/*.deb deb0/
